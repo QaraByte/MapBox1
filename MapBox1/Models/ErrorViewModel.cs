fee360dd1c3dac6c07ba5556e99d11f7eb138173
QaraByte <pchelpkz16@gmail.com>
2022-06-19 22:10:09 +0600

    Add project files.

---
+++ MapBox1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MapBox1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
